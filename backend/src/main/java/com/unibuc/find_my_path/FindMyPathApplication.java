aee70d93a0fc31ca71dd7af80815dab435bc41b7
andreeav19 <andra_368@yahoo.com>
2024-11-30 14:30:24 +0200

    added be project with db connection

---
+++ backend/src/main/java/com/unibuc/find_my_path/FindMyPathApplication.java
@@ -0,0 +1,13 @@
+package com.unibuc.find_my_path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyPathApplication.class, args);
+	}
+
+}
