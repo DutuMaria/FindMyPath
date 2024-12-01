50b1bfb7807019e1a1d55cc2ac6ae3d6491b9c31
BogdanBalan11 <bogdan.balan2021@gmail.com>
2024-12-01 23:06:04 +0200

    [SCRUM-22] add test attempt screen

--- frontend/lib/features/test_attempt/logic/test_attempt_services.dart
+++ frontend/lib/features/test_attempt/logic/test_attempt_services.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:frontend/models/answer.dart';
 import 'package:frontend/models/question.dart';
+import 'package:frontend/models/test_attempt.dart';
+import 'package:frontend/utils/error_handling.dart';
+import 'package:http/http.dart' as http;
 
 class TestAttemptServices {
   List<Question> loadQuestions() {
@@ -24,6 +30,49 @@ class TestAttemptServices {
           Answer(answerId: 8, questionId: 2, answerText: 'Venus'),
         ],
       ),
+      Question(
+        questionId: 3,
+        questionText: 'Which planet is known as the Earth Planet?',
+        answers: [
+          Answer(answerId: 9, questionId: 3, answerText: 'Earth'),
+          Answer(answerId: 10, questionId: 3, answerText: 'Mars'),
+          Answer(answerId: 11, questionId: 3, answerText: 'Jupiter'),
+          Answer(answerId: 12, questionId: 3, answerText: 'Venus'),
+        ],
+      ),
     ];
+  }
+
+  void startTestAttempt({
+    required BuildContext context,
+    required String userId,
+  }) async {
+    try {
+      TestAttempt testAttempt = TestAttempt(
+        testAttemptId: 0,
+        attemptDate: DateTime.now(),
+        experienceRating: null,
+        isCompleted: false,
+        testRating: null,
+        userId: userId,
+      );
+
+      http.Response res = await http.post(
+        Uri.parse('$uri/admin/start-test'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(testAttempt.toJson()),
+      );
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSucces: () {},
+      );
+    } catch (e) {
+      // TODO: show showSnackBar after backend implementation and delete onSuccess
+      // showSnackBar(context, e.toString());
+    }
   }
 }
