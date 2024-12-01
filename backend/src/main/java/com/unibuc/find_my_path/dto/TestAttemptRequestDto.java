d78b1f4f87849a8a097d41e06516face5e1fa0d0
andreeav19 <andra_368@yahoo.com>
2024-12-01 20:51:17 +0200

    added endpoints related to test-attempt

---
+++ backend/src/main/java/com/unibuc/find_my_path/dto/TestAttemptRequestDto.java
@@ -0,0 +1,23 @@
+package com.unibuc.find_my_path.dto;
+
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestAttemptRequestDto {
+    @Min(value = 1, message = "Test rating should be between 1 and 5.")
+    @Max(value = 5, message = "Test rating should be between 1 and 5.")
+    private int testRating;
+
+    @Min(value = 1, message = "Experience rating should be between 1 and 5.")
+    @Max(value = 5, message = "Experience rating should be between 1 and 5.")
+    private int experienceRating;
+}
