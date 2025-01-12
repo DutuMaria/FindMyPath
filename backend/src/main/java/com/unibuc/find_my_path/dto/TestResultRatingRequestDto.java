42dd7d547ed7e9e59f82f2b8a47308dc262dd0c9
andib10 <81637678+andib10@users.noreply.github.com>
2025-01-12 22:07:29 +0200

    fix rating

--- backend/src/main/java/com/unibuc/find_my_path/dto/TestResultRatingRequestDto.java
+++ backend/src/main/java/com/unibuc/find_my_path/dto/TestResultRatingRequestDto.java
@@ -14,5 +14,9 @@ import lombok.Setter;
 public class TestResultRatingRequestDto {
     @Min(value = 1, message = "Rating must be at least 1.")
     @Max(value = 5, message = "Rating must be at most 5.")
-    private Integer rating;
+    private Integer testRating;
+
+    @Min(value = 1, message = "Experience rating must be at least 1.")
+    @Max(value = 5, message = "Experience rating must be at most 5.")
+    private Integer experienceRating;
 }
