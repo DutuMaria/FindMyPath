3f9b3b3b02033ecadce9beaef59a8413bab4b7fb
andreeav19 <andra_368@yahoo.com>
2025-01-19 17:21:15 +0200

    added functions to calculate career compatibility and return sorted results

--- backend/src/main/java/com/unibuc/find_my_path/career_test/com_unibuc_find_my_path_career_test_CareerTestJNI.c
+++ backend/src/main/java/com/unibuc/find_my_path/career_test/com_unibuc_find_my_path_career_test_CareerTestJNI.c
@@ -5,9 +5,12 @@
 
 #include "com_unibuc_find_my_path_career_test_CareerTestJNI.h"
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define SKILL_NUMBER 3
 
 typedef struct {
     float* hardSkills;
@@ -15,28 +18,41 @@ typedef struct {
     float* interests;
 } SkillSet;
 
-char** mapJavaObjectToCStrings(JNIEnv *env, jobject arrayList, int *size) {
-    jclass arrayListClass = (*env)->GetObjectClass(env, arrayList);
-    jmethodID arrayListSizeMethod = (*env)->GetMethodID(env, arrayListClass, "size", "()I");
-    jmethodID getMethod  = (*env)->GetMethodID(env, arrayListClass, "get", "(I)Ljava/lang/Object;");
-    jint arrayListSize = (*env)->CallIntMethod(env, arrayList, arrayListSizeMethod);
-    *size = arrayListSize;
+typedef struct {
+    int careerId;
+    float compatibility;
+} CareerPair;
 
-    char **cString = (char**)malloc(arrayListSize * sizeof(char*));
+// -------- Utils START --------
 
-    for (jint i = 0; i < arrayListSize; i++) {
-        jstring jString = (*env)->CallObjectMethod(env, arrayList, getMethod, i);
-        const char *tempString = (*env)->GetStringUTFChars(env, jString, NULL);
+void printSkillSet(const SkillSet* skillSet, const int skillSetSize, const int hardSkillsSize, const int softSkillsSize, const int interestsSize) {
+    for (int t = 0; t < skillSetSize; t++) {
+        printf("Hard Skills: ");
+        for (int i = 0; i < hardSkillsSize; i++) {
+            printf("%.2f ", skillSet[t].hardSkills[i]);
+        }
+        printf("\n");
 
-        cString[i] = strdup(tempString);
+        printf("Soft Skills: ");
+        for (int i = 0; i < softSkillsSize; i++) {
+            printf("%.2f ", skillSet[t].softSkills[i]);
+        }
+        printf("\n");
 
-        (*env)->ReleaseStringUTFChars(env, jString, tempString);
-        (*env)->DeleteLocalRef(env, jString);
+        printf("Interests: ");
+        for (int i = 0; i < interestsSize; i++) {
+            printf("%.2f ", skillSet[t].interests[i]);
+        }
+        printf("\n");
     }
+}
 
-    (*env)->DeleteLocalRef(env, arrayListClass);
-
-    return cString;
+void printCStringArray(const char** array, const int size, const char* label) {
+    printf("%s:\n", label);
+    for (int i = 0; i < size; i++) {
+        printf("  [%d]: %s\n", i, array[i]);
+    }
+    printf("\n");
 }
 
 void freeCStrings(char** cStrings, const int arraySize) {
@@ -60,6 +76,152 @@ void freeCSkillSet(SkillSet* skillSet, const int skillSetSize) {
         free(skillSet);
     }
 }
+
+// -------- Utils END --------
+
+
+// -------- Calculate Normalized Results START --------
+
+void updateSumSkillSet(const int skillListSize, const float* answersSkillList, float* sumSkillList, float* skillMax) {
+    if (sumSkillList == NULL || answersSkillList == NULL) {
+        printf("MAI AJUNG AICI??\n");
+        return;
+    }
+
+    for (int j = 0; j < skillListSize; j++) {
+        if (answersSkillList[j]) {
+            sumSkillList[j] += answersSkillList[j];
+            if (*skillMax < sumSkillList[j]) *skillMax = sumSkillList[j];
+        }
+    }
+}
+
+void normalizeSkillList(float* skillList, const int skillListSize, const float skillMax) {
+    if (!skillList) return;
+
+    for (int j = 0; j < skillListSize; j++) {
+        skillList[j] /= skillMax;
+    }
+}
+
+SkillSet* obtainResultSkillSet(const SkillSet* answersSkillSet, const int size,
+                              const int hardSkillsSize, const int softSkillsSize, const int interestsSize) {
+    SkillSet* sumSkillSet = (SkillSet*) malloc(sizeof(SkillSet));
+    sumSkillSet->hardSkills = (float*) calloc(hardSkillsSize, sizeof(float));
+    sumSkillSet->softSkills = (float*) calloc(softSkillsSize, sizeof(float));
+    sumSkillSet->interests = (float*) calloc(interestsSize, sizeof(float));
+
+    float hardSkillsMax = 0.0f, softSkillsMax = 0.0f, interestsMax = 0.0f;
+
+    for (int i = 0; i < size; i++) {
+        updateSumSkillSet(hardSkillsSize, answersSkillSet[i].hardSkills,
+            sumSkillSet->hardSkills, &hardSkillsMax);
+        updateSumSkillSet(softSkillsSize, answersSkillSet[i].softSkills,
+            sumSkillSet->softSkills, &softSkillsMax);
+        updateSumSkillSet(interestsSize, answersSkillSet[i].interests,
+            sumSkillSet->interests, &interestsMax);
+    }
+
+    normalizeSkillList(sumSkillSet->hardSkills, hardSkillsSize, hardSkillsMax);
+    normalizeSkillList(sumSkillSet->softSkills, softSkillsSize, softSkillsMax);
+    normalizeSkillList(sumSkillSet->interests, interestsSize, interestsMax);
+
+    return sumSkillSet;
+}
+
+// -------- Calculate Normalized Results END --------
+
+
+// -------- Calculate Career Compatibility (Cosine Similarity) START --------
+float calculateDotProduct(const float* vector1, const float* vector2, const int size) {
+    float result = 0.0f;
+    for (int i = 0; i < size; i++) {
+        result += vector1[i] * vector2[i];
+    }
+    return result;
+}
+
+float calculateNorm(const float* vector, const int size) {
+    float result = 0.0f;
+    for (int i = 0; i < size; i++) {
+        result += vector[i] * vector[i];
+    }
+    return sqrtf(result);
+}
+
+float calculateSkillCompatibility(const float* resultSkill, const float* careerSkill, const int size) {
+    if (size == 0) return 0.0f;
+
+    const float dotProduct = calculateDotProduct(resultSkill, careerSkill, size);
+    const float normResultSkillVector = calculateNorm(resultSkill, size);
+    const float normCareerSkillVector = calculateNorm(careerSkill, size);
+
+    return dotProduct / (normResultSkillVector * normCareerSkillVector);
+}
+
+float* calculateCareersCompatibility(const SkillSet* resultSkillSet,
+    const SkillSet* careersSkillSet, const int careersSkillSetSize,
+    const int hardSkillsSize, const int softSkillsSize, const int interestsSize) {
+    float* careersCompatibility = (float*) malloc(careersSkillSetSize * sizeof(float));
+    for (int i = 0; i < careersSkillSetSize; i++) {
+        const float hardSkillsCompatibility = calculateSkillCompatibility(
+            resultSkillSet->hardSkills,
+            careersSkillSet[i].hardSkills,
+            hardSkillsSize);
+
+        const float softSkillsCompatibility = calculateSkillCompatibility(
+            resultSkillSet->softSkills,
+            careersSkillSet[i].softSkills,
+            softSkillsSize);
+
+        const float interestsCompatibility = calculateSkillCompatibility(
+            resultSkillSet->interests,
+            careersSkillSet[i].interests,
+            interestsSize);
+
+        careersCompatibility[i] =
+            (hardSkillsCompatibility + softSkillsCompatibility + interestsCompatibility) / SKILL_NUMBER;
+    }
+
+    return careersCompatibility;
+}
+
+// -------- Calculate Career Compatibility (Cosine Similarity) END --------
+
+
+// -------- Get All Compatible Career Ids Sorted START --------
+
+int comparePairs(const void* a, const void* b) {
+    const float compatibilityA = ((CareerPair*)a)->compatibility;
+    const float compatibilityB = ((CareerPair*)b)->compatibility;
+    if (compatibilityA < compatibilityB) return 1;
+    if (compatibilityA > compatibilityB) return -1;
+    return 0;
+}
+
+int* getSortedCareerIds(const int* careerIds, const float* careerCompatibility, const int size) {
+    CareerPair* pairs = (CareerPair*) malloc(size * sizeof(CareerPair));
+    for (int i = 0; i < size; i++) {
+        pairs[i].careerId = careerIds[i];
+        pairs[i].compatibility = careerCompatibility[i];
+    }
+
+    qsort(pairs, size, sizeof(CareerPair), comparePairs);
+
+    int* sortedCareerIds = (int*) malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        sortedCareerIds[i] = pairs[i].careerId;
+    }
+
+    free(pairs);
+
+    return sortedCareerIds;
+}
+
+// -------- Get All Compatible Career Ids Sorted START --------
+
+
+// -------- Map Java Objects To C Objects START --------
 
 int indexOfSkillInSkillList(const char* skillName, const char** skillList, const int skillListSize) {
     if (skillName == NULL || skillList == NULL || skillListSize < 1) {
@@ -170,76 +332,90 @@ int* mapJavaObjectsToCIntegers(JNIEnv *env, jobject arrayList, int *size) {
     return integerArray;
 }
 
+char** mapJavaObjectToCStrings(JNIEnv *env, jobject arrayList, int *size) {
+    jclass arrayListClass = (*env)->GetObjectClass(env, arrayList);
+    jmethodID arrayListSizeMethod = (*env)->GetMethodID(env, arrayListClass, "size", "()I");
+    jmethodID getMethod  = (*env)->GetMethodID(env, arrayListClass, "get", "(I)Ljava/lang/Object;");
+    jint arrayListSize = (*env)->CallIntMethod(env, arrayList, arrayListSizeMethod);
+    *size = arrayListSize;
 
-void updateSumSkillSet(const int skillListSize, const float* answersSkillList, float* sumSkillList, float* skillMax) {
-    if (sumSkillList == NULL || answersSkillList == NULL) {
-        printf("MAI AJUNG AICI??\n");
-        return;
+    char **cString = (char**)malloc(arrayListSize * sizeof(char*));
+
+    for (jint i = 0; i < arrayListSize; i++) {
+        jstring jString = (*env)->CallObjectMethod(env, arrayList, getMethod, i);
+        const char *tempString = (*env)->GetStringUTFChars(env, jString, NULL);
+
+        cString[i] = strdup(tempString);
+
+        (*env)->ReleaseStringUTFChars(env, jString, tempString);
+        (*env)->DeleteLocalRef(env, jString);
     }
 
-    for (int j = 0; j < skillListSize; j++) {
-        if (answersSkillList[j]) {
-            sumSkillList[j] += answersSkillList[j];
-            if (*skillMax < sumSkillList[j]) *skillMax = sumSkillList[j];
-        }
-    }
+    (*env)->DeleteLocalRef(env, arrayListClass);
+
+    return cString;
 }
 
-void normalizeSkillList(float* skillList, const int skillListSize, const float skillMax) {
-    if (!skillList) return;
+void mapJavaObjectsToCObjects(JNIEnv *env, jobject jHardSkills, jobject jSoftSkills, jobject jInterests,
+    jobject jAnswersSkillSet, jobject jCareersSkillSet, jobject jCareerIds,
+    const char ***hardSkills, int *hardSkillsSize, const char ***softSkills, int *softSkillsSize, const char ***interests, int *interestsSize,
+    SkillSet **answersSkillSet, int *answersSkillSetSize, SkillSet **careersSkillSet, int *careersSkillSetSize, int **careerIds, int *careerIdsSize) {
 
-    for (int j = 0; j < skillListSize; j++) {
-        skillList[j] /= skillMax;
-    }
+    *hardSkills = (const char**)mapJavaObjectToCStrings(env, jHardSkills, hardSkillsSize);
+    *softSkills = (const char**)mapJavaObjectToCStrings(env, jSoftSkills, softSkillsSize);
+    *interests = (const char**)mapJavaObjectToCStrings(env, jInterests, interestsSize);
+
+    *answersSkillSet = mapJavaObjectsToCSkillSet(env, jAnswersSkillSet, answersSkillSetSize,
+        *hardSkills, *hardSkillsSize, *softSkills, *softSkillsSize, *interests, *interestsSize);
+    *careersSkillSet = mapJavaObjectsToCSkillSet(env, jCareersSkillSet, careersSkillSetSize,
+        *hardSkills, *hardSkillsSize, *softSkills, *softSkillsSize, *interests, *interestsSize);
+
+    *careerIds = mapJavaObjectsToCIntegers(env, jCareerIds, careerIdsSize);
 }
 
-SkillSet* obtainResultSkillSet(const SkillSet* answersSkillSet, const int size,
-                              const int hardSkillsSize, const int softSkillsSize, const int interestsSize) {
-    SkillSet* sumSkillSet = (SkillSet*) malloc(sizeof(SkillSet));
-    sumSkillSet->hardSkills = (float*) calloc(hardSkillsSize, sizeof(float));
-    sumSkillSet->softSkills = (float*) calloc(softSkillsSize, sizeof(float));
-    sumSkillSet->interests = (float*) calloc(interestsSize, sizeof(float));
+// -------- Map Java Objects To C Objects END --------
 
-    float hardSkillsMax = 0.0f, softSkillsMax = 0.0f, interestsMax = 0.0f;
 
-    for (int i = 0; i < size; i++) {
-        updateSumSkillSet(hardSkillsSize, answersSkillSet[i].hardSkills,
-            sumSkillSet->hardSkills, &hardSkillsMax);
-        updateSumSkillSet(softSkillsSize, answersSkillSet[i].softSkills,
-            sumSkillSet->softSkills, &softSkillsMax);
-        updateSumSkillSet(interestsSize, answersSkillSet[i].interests,
-            sumSkillSet->interests, &interestsMax);
-    }
-
-    normalizeSkillList(sumSkillSet->hardSkills, hardSkillsSize, hardSkillsMax);
-    normalizeSkillList(sumSkillSet->softSkills, softSkillsSize, softSkillsMax);
-    normalizeSkillList(sumSkillSet->interests, interestsSize, interestsMax);
-
-    return sumSkillSet;
-}
-
-void mapJavaObjectsToCObjects(JNIEnv *env, jobject jAnswersSkillSet, jobject jCareersSkillSet,
+int* processCareerTestResults(JNIEnv *env, jobject jAnswersSkillSet, jobject jCareersSkillSet,
     jobject jCareerIds, jobject jHardSkills, jobject jSoftSkills, jobject jInterests) {
     int hardSkillsSize, softSkillsSize, interestsSize, answersSkillSetSize, careersSkillSetSize, careerIdsSize;
-    const char** hardSkills = (const char**)mapJavaObjectToCStrings(env, jHardSkills, &hardSkillsSize);
-    const char** softSkills = (const char**)mapJavaObjectToCStrings(env, jSoftSkills, &softSkillsSize);
-    const char** interests = (const char**)mapJavaObjectToCStrings(env, jInterests, &interestsSize);
-    SkillSet* answersSkillSet = mapJavaObjectsToCSkillSet(env, jAnswersSkillSet, &answersSkillSetSize,
-        hardSkills, hardSkillsSize, softSkills, softSkillsSize, interests, interestsSize);
-    SkillSet* careersSkillSet = mapJavaObjectsToCSkillSet(env, jCareersSkillSet, &careersSkillSetSize,
-        hardSkills, hardSkillsSize, softSkills, softSkillsSize, interests, interestsSize);
-    int* careerIds = mapJavaObjectsToCIntegers(env, jCareerIds, &careerIdsSize);
-    
+    const char **hardSkills, **softSkills, **interests;
+    SkillSet *answersSkillSet, *careersSkillSet;
+    int *careerIds;
+
+    mapJavaObjectsToCObjects(env,
+        jHardSkills, jSoftSkills, jInterests,
+        jAnswersSkillSet, jCareersSkillSet, jCareerIds,
+        &hardSkills, &hardSkillsSize, &softSkills, &softSkillsSize, &interests, &interestsSize,
+        &answersSkillSet, &answersSkillSetSize,
+        &careersSkillSet, &careersSkillSetSize,
+        &careerIds, &careerIdsSize
+    );
+
     SkillSet* resultSkillSet = obtainResultSkillSet(answersSkillSet, answersSkillSetSize,
         hardSkillsSize, softSkillsSize, interestsSize);
-    freeCSkillSet(answersSkillSet, answersSkillSetSize);
 
+    float* careersCompatibility = calculateCareersCompatibility((const SkillSet*) resultSkillSet,
+        careersSkillSet, careersSkillSetSize, hardSkillsSize, softSkillsSize, interestsSize);
+
+    int* sortedCareerIds = getSortedCareerIds(careerIds, careersCompatibility, careersSkillSetSize);
+
+    free(hardSkills);
+    free(softSkills);
+    free(interests);
+    free(careerIds);
+    free(careersCompatibility);
+    freeCSkillSet(resultSkillSet, 1);
+    freeCSkillSet(answersSkillSet, answersSkillSetSize);
+    freeCSkillSet(careersSkillSet, careersSkillSetSize);
+
+    return sortedCareerIds;
 }
 
 JNIEXPORT jobject JNICALL Java_com_unibuc_find_1my_1path_career_1test_CareerTestJNI_processCareerTestResults
   (JNIEnv *env, jobject thisJObj,  jobject jAnswersSkillSet, jobject jCareerSkillSet, jobject jCareerIds,
       jobject jHardSkills, jobject jSoftSkills, jobject jInterests) {
-    mapJavaObjectsToCObjects(env, jAnswersSkillSet, jCareerSkillSet,
+    long* careerTestResults = (long*) processCareerTestResults(env, jAnswersSkillSet, jCareerSkillSet,
         jCareerIds, jHardSkills, jSoftSkills, jInterests);
 
     return NULL;
